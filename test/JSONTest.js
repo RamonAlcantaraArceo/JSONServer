374661d68eb296ab8647524aa71bf705aad265e4
Ramón Alcántara <ramon.alcantara@move.com>
2017-11-29 11:54:34 -0500

    add test skeleton
    Add a test skeleton to use with Mocha/Chai

---
+++ test/JSONTest.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+
+describe ('JSON', function(){
+	describe('Dummy', function(){
+		it('Dummy should assert for fun', function(){
+			assert.equal(false, false);
+		})
+	})
+})
