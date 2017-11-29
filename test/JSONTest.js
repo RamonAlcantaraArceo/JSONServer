dec7f1753b8b56678ef0c758a619031995c5acf5
Ramón Alcántara <ramon.alcantara@move.com>
2017-11-29 12:57:07 -0500

    Modify to hit the JSON Server
    Now the test is hitting the JSON Server. One step closer to actual monkey business.

--- test/JSONTest.js
+++ test/JSONTest.js
@@ -1,9 +1,22 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
+var request = require("request");
+
+const baseUrl = "https://my-json-server.typicode.com/ramon-alcantara-move/JSONServer"
 
 describe ('JSON', function(){
 	describe('Dummy', function(){
 		it('Dummy should assert for fun', function(){
 			assert.equal(false, false);
+		})
+		it('Dummy asking for a url', function(done){
+			request.get({url: baseUrl + "/posts"}, 
+				function (error, response, body){
+					expect(response.statusCode).to.equal(200);
+					console.log(body);
+					done();
+			})
+			assert.equal(true, true);
 		})
 	})
 })
